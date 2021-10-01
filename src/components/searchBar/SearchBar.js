d8f5e5b320b7f5fc8f41bb8e9ab1f4ca8bc2edd9
Yann David <yann.collindavid@gmail.com>
2021-10-01 16:36:12 +0200

    search bar cleaned

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -8,39 +8,31 @@ import { useState, useEffect, useRef } from 'react';
 import { TiDelete } from "react-icons/ti"
 import { RiArrowDropDownLine } from 'react-icons/ri'
 
-// let fakeTerritories = ["Biovallée","Diois","Crest","Valence"]
+// Import fonctions
+import { recommendation, handleSearch, addTerritoryToAnalysis, namingLocation, removerTerritoryFromAnalysis, openInfo,styleModal } from './SearchFunctions'
+
 
 const SearchBar = ({API_URL, setDashboard, territories, setTerritories}) => {
+    // Ref definitions
     const SEARCH_BAR_REF = useRef(null)
     const LIST_ITEM_REF = useRef(null)
+
+    // State variables
     const [modal,setModal] = useState({
         clientX:0,
         clientY:0,
         open:false,
     })
-
     const [query, setQuery] = useState("")
     const [listTerritories, setListTerritories] = useState([])
     const [panelTerritories, setPanelTerritories] = useState(false)
 
 
+    // Effects 
     useEffect(() => {
-        const  recommendation = async (query) =>{
-            const response = await fetch(`${API_URL}/getLocation`, {
-                body: JSON.stringify({"location":query}),
-                method: "POST",
-                headers: {
-                    // Authorization: bearer,
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                  },
-
-              });
-              const data = await response.json()
-           
-            setListTerritories(data)   
+        if(query.length%2===0 & query!==""){
+            recommendation(query,API_URL,setListTerritories)
         }
-        recommendation(query)
        
     }, [query,API_URL])
     
@@ -48,101 +40,39 @@ const SearchBar = ({API_URL, setDashboard, territories, setTerritories}) => {
         if(territories.length > 1 & LIST_ITEM_REF.current.offsetWidth>=SEARCH_BAR_REF.current.offsetWidth*0.30){
             setPanelTerritories(true)   
         }
-        
-    },[territories])
-
-    // Function handling search query
-    const handleSearch = (e) =>{
-        e.preventDefault();
-        setDashboard(true)
-    
-    }
-
-    const addTerritoryToAnalysis = (e,t) => {
-        e.preventDefault();
-        setTerritories(territories => [...territories,t])
-        setListTerritories(listTerritories.filter(ter=>ter!==t))  
-        setQuery("")
-        
-    }
-
-    const removerTerritoryFromAnalysis = (e,t) =>{
-        e.preventDefault();
-        if(territories.length-1===0){
-            setModal({open:false})
-        }
-        if(territories.length-2===0){
-            setPanelTerritories(false)
-            setModal({open:false})
-   
-        }
-        setTerritories(territories.filter(ter=>ter!==t))
-      
-        if(LIST_ITEM_REF.current.offsetWidth<=SEARCH_BAR_REF.current.offsetWidth*0.3 ){
-            setPanelTerritories(false)   
-        }
-    }
-
-
-
-
-    const namingLocation = (t,shorten=false) =>{
-        if(t.LIBGEO[0].length > 10 & shorten===true){
-            if(territories.length >1){
-                return t.LIBGEO[0].substring(0,6)+'... ('+t.CODGEO[0].substring(0,2)+')'
-            }else{
-                return t.LIBGEO[0].substring(0,10)+'... ('+t.CODGEO[0].substring(0,2)+')'
-            }
+        if(territories.length>=1){
+            setDashboard(true)
         }else{
-           return t.LIBGEO[0]+' ('+t.CODGEO[0].substring(0,2)+')'
+            setDashboard(false)
         }
-    }
+        
+    },[territories,setDashboard,recommendation])
 
-    const openInfo = (e) =>{
-        setModal({
-            clientX:SEARCH_BAR_REF.current.offsetLeft,
-            clientY:SEARCH_BAR_REF.current.offsetTop,
-            open:true,
-        })
-    }
-
-    const styleModal = (modal) =>{
-        // console.log(SEARCH_BAR_REF.current.offsetWidth)
-        const width = SEARCH_BAR_REF.current.offsetWidth*0.25
-        return {
-            width:width,
-            top:modal.clientY,
-            left:modal.clientX,
-            borderRadius:7
-        }
-    }
-
-  
+   
     return (
         <div className="search-zone">
             {/* Search bar strictly speaking */}
-            <form onSubmit={(e)=>handleSearch(e)} className="search-form">
+            <form onSubmit={(e)=>handleSearch(e,query,API_URL,setListTerritories)} className="search-form">
                 <div className="search-bar" ref={SEARCH_BAR_REF}>
                     <div className="selected-list" 
-                    // style={{maxWidth: barWidth*0.30}}
                     ref={LIST_ITEM_REF}>
                         {panelTerritories?(
                             <div className="selected-list-min">
                             <span 
                                 key={19}
                                 className="selected">
-                                    {namingLocation(territories[0],true)}
+                                    {namingLocation(territories[0],true,territories)}
                                     <TiDelete className="delete-territory" 
-                                    onClick = {e => removerTerritoryFromAnalysis(e,territories[0])}
+                                    onClick = {e => removerTerritoryFromAnalysis(e,territories[0],setModal,setPanelTerritories,setTerritories,territories,LIST_ITEM_REF,SEARCH_BAR_REF)}
                                     />
                             </span>
-                            <span onClick={(e)=>openInfo(e)}
+                            <span onClick={(e)=>openInfo(e,SEARCH_BAR_REF,setModal)}
                                 key={10}
                                 className="selected aggregate">
                                     + {territories.length-1} 
                             </span>
                             <RiArrowDropDownLine  
-                            onClick={(e)=>openInfo(e)}
+                            onClick={(e)=>openInfo(e,SEARCH_BAR_REF,setModal)}
                             size="30px" className="drop-btn" />
                             </div>
                         ):(
@@ -151,9 +81,9 @@ const SearchBar = ({API_URL, setDashboard, territories, setTerritories}) => {
                         return <span 
                         key={i}
                         className="selected">
-                            {namingLocation(t,true)}
+                            {namingLocation(t,true,territories)}
                             <TiDelete className="delete-territory" 
-                            onClick = {e => removerTerritoryFromAnalysis(e,t)}
+                            onClick = {e => removerTerritoryFromAnalysis(e,t,setModal,setPanelTerritories,setTerritories,territories,LIST_ITEM_REF,SEARCH_BAR_REF)}
                             />
                         </span>
                     })
@@ -162,9 +92,8 @@ const SearchBar = ({API_URL, setDashboard, territories, setTerritories}) => {
                     <input className="search-value" type="input"
                     onChange={(dta) => setQuery(dta.target.value)}
                     value = {query}
-                    placeholder="Cherchez et ajoutez des territoires à votre tableau de bord"/>   
+                    placeholder="🔍️  Cherchez et ajoutez des territoires à votre tableau de bord (nom de commune, code postal..)"/>   
                 </div>
-                <input className="search-go" type="submit" value="GO"/>
             </form>
 
             {/* Drop down of suggestions */}
@@ -172,16 +101,16 @@ const SearchBar = ({API_URL, setDashboard, territories, setTerritories}) => {
                         {query!==""&&listTerritories.map((t,i)=>{
                             return <span 
                             key={i}
-                            onClick={e=>addTerritoryToAnalysis(e,t)}
+                            onClick={e=>addTerritoryToAnalysis(e,t,setTerritories,setListTerritories,setQuery,territories,listTerritories)}
                             className="item-list proposition">
-                                {namingLocation(t)}
+                                {namingLocation(t,false,territories)}
                             </span>
                         })}
             </div> 
 
             {/* Modal of Cities added */}
             {modal.open && 
-                <div className="modal" style={styleModal(modal)}>
+                <div className="modal" style={styleModal(modal,SEARCH_BAR_REF)}>
                     <div className="drop-header">
                         <h4 className="drop-title" >Territoires ajoutés : </h4>
                         <RiArrowDropDownLine 
@@ -190,16 +119,18 @@ const SearchBar = ({API_URL, setDashboard, territories, setTerritories}) => {
                         className='drop-btn close' />
     
                     </div>
-                    <div className="column" >
-                    {territories.map((t,i)=>{
-                        return <span 
-                        key={i}
-                        className="selected col-items">
-                            {namingLocation(t)}
-                            <TiDelete className="delete-territory" 
-                            onClick = {e => removerTerritoryFromAnalysis(e,t)}
-                            />
+                    <div className="column " >
+                    {territories.sort((a,b)=>a.LIBGEO[0]>b.LIBGEO[0] ? 1 : -1).map((t,i)=>{
+                        return <div key={i} className="selected sel-modal">
+                        <span 
+                        className=" col-items">
+                            {namingLocation(t,false,territories)}
                         </span>
+                            <TiDelete className="delete-territory" 
+                            onClick = {e => removerTerritoryFromAnalysis(e,t,setModal,setPanelTerritories,setTerritories,territories,LIST_ITEM_REF,SEARCH_BAR_REF)}
+                            />
+
+                        </div>
                     })}
                     </div>
     
