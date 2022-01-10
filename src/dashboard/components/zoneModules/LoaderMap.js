e38cf086e0b9066211b4c513bfd79c40b5807c3f
Yann David <yann.collindavid@gmail.com>
2022-01-10 18:35:27 +0100

    maj skeleton map

--- src/dashboard/components/zoneModules/LoaderMap.js
+++ src/dashboard/components/zoneModules/LoaderMap.js
@@ -3,15 +3,15 @@ import ContentLoader from "react-content-loader";
 const LoaderMap = (props) => (
   <ContentLoader
     speed={2}
-    width={400}
-    height={400}
-    viewBox="0 0 400 400"
+    width={600}
+    height={500}
+    viewBox="0 0 600 500"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
     {...props}
   >
     <rect x="2" y="11" rx="0" ry="0" width="86" height="32" />
-    <rect x="4" y="57" rx="0" ry="0" width="389" height="333" />
+    <rect x="2" y="57" rx="0" ry="0" width="550" height="400" />
   </ContentLoader>
 );
 
