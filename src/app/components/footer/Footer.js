565eebac903dd98c4cbc4858effe06ec45537795
yanndav <yann.collindavid@gmail.com>
2022-03-18 10:33:24 +0100

    changement nom version

--- src/app/components/footer/Footer.js
+++ src/app/components/footer/Footer.js
@@ -33,7 +33,7 @@ const Footer = () => {
         />
       </a>
 
-      <p>Comparater - Version Alpha 3 - Comparo - CIRED-ENPC 2022</p>
+      <p>Comparater - Version Pre-sortie - CIRED-ENPC 2022</p>
     </div>
   );
 };
