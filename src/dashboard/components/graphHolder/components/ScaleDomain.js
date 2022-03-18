941991fd76d81746125f8dd50382443d0343bb89
yanndav <yann.collindavid@gmail.com>
2022-03-18 10:28:59 +0100

    remove dashboard

--- src/dashboard/components/graphHolder/components/ScaleDomain.js
+++
@@ -1,9 +0,0 @@
-import { min,max } from "d3-array"
-const scaleDomain = (data,variable) =>{
-    let mini = min(data, d => d[variable])
-    let maxi = max(data, d => d[variable])
-
-    return (maxi-mini)*0.05
-}
-
-export default scaleDomain
